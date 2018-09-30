484d72d325f9b74d38571132a6ccefc3a330914c
Mura Li <mura_li@example.com>
2018-09-30 10:06:56 +0800

    Add support for sqlite3_unlock_notify

--- sqlite3_opt_unlock_notify.go
+++ sqlite3_opt_unlock_notify.go
@@ -21,6 +21,7 @@ import (
 	"unsafe"
 )
 
+<<<<<<< HEAD
 type unlockNotification struct {
 	notify chan struct{}
 	lock   sync.Mutex
@@ -33,6 +34,46 @@ func unlock_notify_callback(pargv unsafe.Pointer, argc C.int) {
 	for i := 0; i < int(argc); i++ {
 		un := lookupHandle(v[i]).(unlockNotification)
 		un.notify <- struct{}{}
+=======
+type notifications struct {
+	sync.Mutex
+	seqnum uint
+	table  map[uint]chan struct{}
+}
+
+func (n *notifications) new() (uint, <-chan struct{}) {
+	n.Lock()
+	defer n.Unlock()
+
+	c := make(chan struct{})
+	h := n.seqnum
+	n.table[uint(h)] = c
+	n.seqnum++
+	return h, c
+}
+
+func (n *notifications) remove(h uint) chan<- struct{} {
+	n.Lock()
+	defer n.Unlock()
+
+	c, ok := n.table[uint(h)]
+	if !ok {
+		panic("non-existent notification entry")
+	}
+	return c
+}
+
+var _notifications notifications = notifications{table: make(map[uint]chan struct{})}
+
+//export unlock_notify_callback
+func unlock_notify_callback(argvv unsafe.Pointer, argc C.int) {
+	// NOTE: the order of nofitications is FILO. The first locked would be last unlocked.
+	for i := 0; i < int(argc); i++ {
+		argv := *((*(*[1 << 30]*[1]uint)(argvv))[i])
+		h := argv[0]
+		c := _notifications.remove(h)
+		close(c)
+>>>>>>> Add support for sqlite3_unlock_notify
 	}
 }
 
@@ -40,6 +81,7 @@ var notifyMutex sync.Mutex
 
 //export unlock_notify_wait
 func unlock_notify_wait(db *C.sqlite3) C.int {
+<<<<<<< HEAD
 	var un unlockNotification
 	un.notify = make(chan struct{})
 	defer close(un.notify)
@@ -49,5 +91,13 @@ func unlock_notify_wait(db *C.sqlite3) C.int {
 		return rv
 	}
 	<-un.notify
+=======
+	h, notify := _notifications.new()
+	argv := [1]uint{h}
+	if rv := C.sqlite3_unlock_notify(db, (*[0]byte)(C._unlock_notify_callback), unsafe.Pointer(&argv)); rv != C.SQLITE_OK {
+		return rv
+	}
+	<-notify
+>>>>>>> Add support for sqlite3_unlock_notify
 	return C.SQLITE_OK
 }
--- sqlite3_opt_unlock_notify_test.go
+++ sqlite3_opt_unlock_notify_test.go
@@ -16,7 +16,11 @@ import (
 	"time"
 )
 
+<<<<<<< HEAD
 func TestTableLockedError(t *testing.T) {
+=======
+func TestUnlockNotify(t *testing.T) {
+>>>>>>> Add support for sqlite3_unlock_notify
 	tempFilename := TempFilename(t)
 	defer os.Remove(tempFilename)
 	dsn := fmt.Sprintf("file:%s?cache=shared&mode=rwc&_busy_timeout=%d", tempFilename, 500)
@@ -75,3 +79,72 @@ func TestTableLockedError(t *testing.T) {
 	wg.Wait()
 
 }
+<<<<<<< HEAD
+=======
+
+func TestUnlockNotifyMany(t *testing.T) {
+	tempFilename := TempFilename(t)
+	defer os.Remove(tempFilename)
+	dsn := fmt.Sprintf("file:%s?cache=shared&mode=rwc&_busy_timeout=%d", tempFilename, 500)
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		t.Fatal("Failed to open database:", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE foo(id INTEGER, status INTEGER)")
+	if err != nil {
+		t.Fatal("Failed to create table:", err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatal("Failed to begin transaction:", err)
+	}
+
+	_, err = tx.Exec("INSERT INTO foo(id, status) VALUES(1, 100)")
+	if err != nil {
+		t.Fatal("Failed to insert null:", err)
+	}
+
+	_, err = tx.Exec("UPDATE foo SET status = 200 WHERE id = 1")
+	if err != nil {
+		t.Fatal("Failed to update table:", err)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	timer := time.NewTimer(500 * time.Millisecond)
+	go func() {
+		<-timer.C
+		err := tx.Commit()
+		if err != nil {
+			t.Fatal("Failed to commit transaction:", err)
+		}
+		wg.Done()
+	}()
+
+	const concurrentQueries = 100
+	wg.Add(concurrentQueries)
+	for i := 0; i < concurrentQueries; i++ {
+		go func() {
+			rows, err := db.Query("SELECT count(*) from foo")
+			if err != nil {
+				t.Fatal("Unable to query foo table:", err)
+			}
+
+			if rows.Next() {
+				var count int
+				if err := rows.Scan(&count); err != nil {
+					t.Fatal("Failed to Scan rows", err)
+				}
+			}
+			if err := rows.Err(); err != nil {
+				t.Fatal("Failed at the call to Next:", err)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+>>>>>>> Add support for sqlite3_unlock_notify
